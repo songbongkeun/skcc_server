e3ffaac7b939210d6bb3e06c8ac2f36c96b78055
songbongkeun <songbongkeun@naver.com>
2013-11-18 00:23:56 +0900

    commitkkk

--- skcc_springserver/src/main/java/com/skcc/springserver/webservice/GetObjectTestInterface.java
+++ skcc_springserver/src/main/java/com/skcc/springserver/webservice/GetObjectTestInterface.java
@@ -1,7 +1,6 @@
 package com.skcc.springserver.webservice;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
